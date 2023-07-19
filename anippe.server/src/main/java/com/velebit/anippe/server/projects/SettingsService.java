83e19db2654684ef0bfcc7c8de6ffe23dbfe8aa6
lukacavic <luka.cavic@gmail.com>
2023-07-19 17:36:06 +0200

    Remove generated permissions

--- anippe.server/src/main/java/com/velebit/anippe/server/projects/SettingsService.java
+++ anippe.server/src/main/java/com/velebit/anippe/server/projects/SettingsService.java
@@ -8,37 +8,21 @@ import org.eclipse.scout.rt.security.ACCESS;
 public class SettingsService implements ISettingsService {
     @Override
     public SettingsFormData prepareCreate(SettingsFormData formData) {
-        if (!ACCESS.check(new CreateSettingsPermission())) {
-            throw new VetoException(TEXTS.get("AuthorizationFailed"));
-        }
-// TODO [lukacavic] add business logic here.
         return formData;
     }
 
     @Override
     public SettingsFormData create(SettingsFormData formData) {
-        if (!ACCESS.check(new CreateSettingsPermission())) {
-            throw new VetoException(TEXTS.get("AuthorizationFailed"));
-        }
-// TODO [lukacavic] add business logic here.
         return formData;
     }
 
     @Override
     public SettingsFormData load(SettingsFormData formData) {
-        if (!ACCESS.check(new ReadSettingsPermission())) {
-            throw new VetoException(TEXTS.get("AuthorizationFailed"));
-        }
-// TODO [lukacavic] add business logic here.
         return formData;
     }
 
     @Override
     public SettingsFormData store(SettingsFormData formData) {
-        if (!ACCESS.check(new UpdateSettingsPermission())) {
-            throw new VetoException(TEXTS.get("AuthorizationFailed"));
-        }
-// TODO [lukacavic] add business logic here.
         return formData;
     }
 }
