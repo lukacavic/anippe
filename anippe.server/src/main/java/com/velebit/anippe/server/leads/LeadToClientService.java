f540e420da80f506987658b9b1ea973a317f7c28
lukacavic <luka.cavic@gmail.com>
2023-10-19 17:20:29 +0200

    First implementation of converting lead to client

--- anippe.server/src/main/java/com/velebit/anippe/server/leads/LeadToClientService.java
+++ anippe.server/src/main/java/com/velebit/anippe/server/leads/LeadToClientService.java
@@ -1,12 +1,14 @@
 package com.velebit.anippe.server.leads;
 
+import com.velebit.anippe.server.AbstractService;
 import com.velebit.anippe.shared.leads.ILeadToClientService;
 import com.velebit.anippe.shared.leads.LeadToClientFormData;
+import org.eclipse.scout.rt.platform.holders.IntegerHolder;
 import org.eclipse.scout.rt.platform.holders.NVPair;
 import org.eclipse.scout.rt.platform.holders.StringHolder;
 import org.eclipse.scout.rt.server.jdbc.SQL;
 
-public class LeadToClientService implements ILeadToClientService {
+public class LeadToClientService extends AbstractService implements ILeadToClientService {
     @Override
     public LeadToClientFormData prepareCreate(LeadToClientFormData formData) {
         StringHolder holder = new StringHolder();
@@ -41,17 +43,93 @@ public class LeadToClientService implements ILeadToClientService {
         return formData;
     }
 
+    private void markContactAsPrimaryForClient(Integer clientId, Integer contactId) {
+        StringBuffer varname1 = new StringBuffer();
+        varname1.append("UPDATE clients ");
+        varname1.append("SET    primary_contact_id = :contactId ");
+        varname1.append("WHERE  id = :clientId");
+        SQL.update(varname1.toString(), new NVPair("clientId", clientId), new NVPair("contactId", contactId));
+    }
+
+    private void addClientIdToLead(Integer clientId, Integer leadId) {
+        StringBuffer varname1 = new StringBuffer();
+        varname1.append("UPDATE leads ");
+        varname1.append("SET    client_id = :clientId ");
+        varname1.append("WHERE  id = :leadId");
+        SQL.update(varname1.toString(), new NVPair("clientId", clientId), new NVPair("leadId", leadId));
+    }
+
     @Override
     public LeadToClientFormData create(LeadToClientFormData formData) {
-        //Kreiraj kompaniju po nazivu + adresa polja i ostalo
+        Integer clientId = createClient(formData);
 
-        //Kreiraj kontakt i vezi na kompaniju
+        Integer contactId = createContact(formData, clientId);
 
-        //Stavi kontakt da je primarni za kompaniju.
+        markContactAsPrimaryForClient(clientId, contactId);
 
-        //Prebaci sa lead-a sve napomene, privitke i preostale zadatke ako je označeno u formi.
+        addClientIdToLead(clientId, formData.getLeadId());
 
         return formData;
+    }
+
+    private Integer createContact(LeadToClientFormData formData, Integer clientId) {
+        IntegerHolder holder = new IntegerHolder();
+
+        StringBuffer varname1 = new StringBuffer();
+        varname1.append("INSERT INTO contacts ");
+        varname1.append("            (first_name, ");
+        varname1.append("             last_name, ");
+        varname1.append("             client_id, ");
+        varname1.append("             email, ");
+        varname1.append("             phone, ");
+        varname1.append("             position, ");
+        varname1.append("             created_at, ");
+        varname1.append("             organisation_id) ");
+        varname1.append("VALUES      (:FirstName, ");
+        varname1.append("             :LastName, ");
+        varname1.append("             :clientId, ");
+        varname1.append("             :Email, ");
+        varname1.append("             :Phone, ");
+        varname1.append("             :Position, ");
+        varname1.append("             Now(), ");
+        varname1.append("             :organisationId) RETURNING id INTO :holder");
+        SQL.selectInto(varname1.toString(), formData,
+                new NVPair("organisationId", getCurrentOrganisationId()),
+                new NVPair("holder", holder),
+                new NVPair("clientId", clientId));
+
+        return holder.getValue();
+    }
+
+    private Integer createClient(LeadToClientFormData formData) {
+        IntegerHolder holder = new IntegerHolder();
+
+        StringBuffer varname1 = new StringBuffer();
+        varname1.append("INSERT INTO clients ");
+        varname1.append("            (NAME, ");
+        varname1.append("             address, ");
+        varname1.append("             city, ");
+        varname1.append("             postal_code, ");
+        varname1.append("             country_id, ");
+        varname1.append("             website, ");
+        varname1.append("             phone, ");
+        varname1.append("             active, ");
+        varname1.append("             created_at, ");
+        varname1.append("             organisation_id) ");
+        varname1.append("VALUES      (:Company, ");
+        varname1.append("             :Address, ");
+        varname1.append("             :City, ");
+        varname1.append("             :PostalCode, ");
+        varname1.append("             :Country, ");
+        varname1.append("             :Website, ");
+        varname1.append("             :Phone, ");
+        varname1.append("             true, ");
+        varname1.append("             Now(), ");
+        varname1.append("             :organisationId) ");
+        varname1.append("returning id INTO :holder");
+        SQL.selectInto(varname1.toString(), formData, new NVPair("organisationId", getCurrentOrganisationId()), new NVPair("holder", holder));
+
+        return holder.getValue();
     }
 
 }
